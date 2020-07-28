95ed10c21e332ff57672cf9bc9aca1c7d66a56e8
Nikita <fidg37@gmail.com>
2020-07-28 17:56:12 +0200

    описал функцию для рендеринга элементов на страницу

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const createUserRankTemplate = () => {
+  return (
+    `<section class="header__profile profile">
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+    </section>`
+  );
+};
+
+const render = (container, template, place) => {
+  container.insertAjacentHTML(place, template);
+};
