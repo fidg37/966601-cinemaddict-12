77fb68c1300ac12a84725e50cc9bd6f1680d7f74
Nikita <fidg37@gmail.com>
2020-07-28 21:15:14 +0200

    описал функцию, возвращающую разметку комментария, добавил комментарии на страницу

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 const CARD_COUNT = 5;
 const EXTRA_COUNT = 2;
+const COMMENTS_COUNT = 4;
 
 const INSERT_PLACE = {
   BFE: `beforeend`,
@@ -261,3 +262,25 @@ const createPopupControlTemplate = () => (
 );
 
 render(filmInfoBlock, createPopupControlTemplate(), INSERT_PLACE.BFE);
+
+const createCommentTemplate = () => (
+  `<li class="film-details__comment">
+    <span class="film-details__comment-emoji">
+      <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
+    </span>
+    <div>
+      <p class="film-details__comment-text">Interesting setting and a good cast</p>
+      <p class="film-details__comment-info">
+        <span class="film-details__comment-author">Tim Macoveev</span>
+        <span class="film-details__comment-day">2019/12/31 23:59</span>
+        <button class="film-details__comment-delete">Delete</button>
+      </p>
+    </div>
+  </li>`
+);
+
+const commentsList = popup.querySelector(`.film-details__comments-list`);
+
+for (let i = 0; i < COMMENTS_COUNT; i++) {
+  render(commentsList, createCommentTemplate(), INSERT_PLACE.BFE);
+}
